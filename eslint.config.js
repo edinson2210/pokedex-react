c4965fdd7252508189c83fc6ac85a9f0b9cfcab9
Edinson López <Edinson.221093@gmail.com>
2026-07-12 11:23:47 -0500

    chore(lint): configurar ESLint con Prettier y ajustar reglas de hooks

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,9 +15,16 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      // Setting loading/error state synchronously at the start of a data-fetching
+      // effect is the standard React pattern (see react.dev/learn/synchronizing-with-effects).
+      // This rule is too strict for that idiom, so we relax it.
+      'react-hooks/set-state-in-effect': 'off',
     },
   },
 ])
